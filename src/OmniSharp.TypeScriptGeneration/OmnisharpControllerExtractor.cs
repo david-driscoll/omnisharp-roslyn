2895a4b804bd2699b69edc295414e127cb718413
David Driscoll <david.driscoll@gmail.com>
2015-04-24 21:52:33 -0400

    Added initial run of typescript generation.

---
+++ src/OmniSharp.TypeScriptGeneration/OmnisharpControllerExtractor.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using OmniSharp.Models;
+
+namespace OmniSharp.TypeScriptGeneration
+{
+    public static class OmnisharpControllerExtractor
+    {
+        public static string GetInterface()
+        {
+            var methods = "        " + string.Join("\n        ", GetInterfaceMethods()) + "\n";
+
+            return $"declare module {nameof(OmniSharp)} {{\n    interface Api {{\n{methods}    }}\n}}";
+        }
+
+        private static IEnumerable<string> GetInterfaceMethods()
+        {
+            foreach (var method in GetControllerMethods())
+            {
+                var returnType = method.ReturnType;
+                if (method.ReturnsArray)
+                    returnType += "[]";
+                if (method.RequestType != null)
+                {
+                        yield return $"(action: \"{method.Action}\", request: {method.RequestType.FullName}): Promise<{returnType}>;";
+                }
+                else
+                {
+                    yield return $"(action: \"{method.Action}\"): Promise<{returnType}>";
+                }
+            }
+        }
+
+        class MethodResult
+        {
+            public string Action { get; set; }
+            public Type RequestType { get; set; }
+            public string ReturnType { get; set; }
+            public bool ReturnsArray { get; set; }
+        }
+
+        private static IEnumerable<MethodResult> GetControllerMethods()
+        {
+            var controller = typeof(OmnisharpController);
+            foreach (var method in controller.GetTypeInfo().DeclaredMethods.Where(z => z.IsPublic))
+            {
+                var attribute = method.GetCustomAttribute<HttpPostAttribute>();
+                if (attribute != null)
+                {
+                    var parameters = method.GetParameters();
+                    var param = parameters.Length == 1 ? parameters[0].ParameterType : null;
+
+                    var returnType = method.ReturnType;
+                    var returnsArray = false;
+                    if (returnType.Name.StartsWith(nameof(Task), StringComparison.Ordinal))
+                    {
+                        returnType = returnType.GetGenericArguments().First();
+                    }
+                    if (returnType.Name.StartsWith(nameof(IEnumerable), StringComparison.Ordinal))
+                    {
+                        returnsArray = true;
+                        returnType = returnType.GetGenericArguments().First();
+                    }
+
+                    string returnString = "any";
+                    if (returnType != null && returnType.FullName.StartsWith(InferNamespace(typeof(Request)), StringComparison.Ordinal))
+                    {
+                        returnString = returnType.FullName;
+                    }
+
+                    if (returnType == typeof(Boolean))
+                    {
+                        returnString = nameof(Boolean).ToLowerInvariant();
+                    }
+
+                    yield return new MethodResult()
+                    {
+                        RequestType = param,
+                        ReturnType = returnString,
+                        ReturnsArray = returnsArray,
+                        Action = attribute.Template
+                    };
+                }
+            }
+        }
+
+        internal static string InferNamespace(Type type)
+        {
+            var pieces = type.FullName.Split('.');
+            return string.Join(".", pieces.Take(pieces.Length - 1)) + ".";
+        }
+    }
+}
---
+++ src/OmniSharp.TypeScriptGeneration/TsFluentFormatters.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using OmniSharp.Models;
+using OmniSharp.Stdio.Protocol;
+using TypeLite.TsModels;
+
+namespace OmniSharp.TypeScriptGeneration
+{
+    public static class TsFluentFormatters
+    {
+        public static string FormatPropertyType(TsProperty type, string memberTypeName)
+        {
+            if (type.MemberInfo.DeclaringType.GetProperty(type.MemberInfo.Name).PropertyType == typeof(IDictionary<string, string>))
+            {
+                return "{ [key: string]: string }";
+            }
+
+            if (type.MemberInfo.DeclaringType.GetProperty(type.MemberInfo.Name).PropertyType == typeof(Guid))
+            {
+                return "string";
+            }
+
+            if (type.MemberInfo.DeclaringType.GetProperty(type.MemberInfo.Name).PropertyType == typeof(Stream))
+            {
+                return "any";
+            }
+
+            return memberTypeName;
+        }
+
+        public static string FormatPropertyName(TsProperty property)
+        {
+            // These are mapped as arguments from the client side.
+            if (property.Name == nameof(RequestPacket.ArgumentsStream))
+            {
+                return "Arguments";
+            }
+
+            // Request type arguments are optional
+            // TODO: Leverage [Required] to know what is needed and not?
+            if (property.MemberInfo.DeclaringType.Name != nameof(Request) &&
+                !property.MemberInfo.DeclaringType.Name.Contains(nameof(Packet)) &&
+                property.MemberInfo.DeclaringType.Name.Contains(nameof(Request)))
+            {
+                return $"{property.Name}?";
+            }
+
+            return property.Name;
+        }
+    }
+}
